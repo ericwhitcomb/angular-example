bcb34a55fda816134f0b363a4faddf30057abdce
Eric Whitcomb <eric.whitcomb@gmail.com>
2022-03-02 19:49:45 -0600

    Create component hello-world
    Implements ngOnInit, ngOnDestroy.
    Turned strict off in tsconfig.json.
    Used setInterval.

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.2.5.
 
+Following Youtube tutorial [Angular Tutorial for Beginners](https://www.youtube.com/watch?v=AAu8bjj6-UI) from freeCodeCamp.org channel.
+
 ## Development server
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AnyCatcher } from 'rxjs/internal/AnyCatcher';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'angular-example';
+export class AppComponent implements OnInit, OnDestroy {
+  title: 'angular-example';
+  intervalSub: ReturnType<typeof setInterval>;
+
+  ngOnInit(): void {
+    this.intervalSub = setInterval(() => {
+      console.log('Hello from ngOnInit');
+    }, 1000);
+  }
+
+  ngOnDestroy(): void {
+    if (this.intervalSub) {
+      clearInterval(this.intervalSub);
+    }
+  }
 }
